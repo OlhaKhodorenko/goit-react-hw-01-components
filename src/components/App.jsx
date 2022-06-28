f7a5dd6a9fdeadfc7fe9bf1b2b10a0f9400c8fc0
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-06-28 12:09:41 +0300

    add prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,24 @@ import { Statistics } from './statistics/statistics';
 import { FriendList } from './friendList/friendList';
 import { TransactionHistory } from './transaction/transactionHistory';
 
-import friends from "data/friends.json";
-import user from "data/user.json";
-import data from "data/data.json";
-import transactions from "data/transactions.json";
+import friends from 'data/friends.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
     <div>
-    <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-  <Statistics title="Upload stats" stats={data} />
-  <FriendList friends={friends} />
-  <TransactionHistory items={transactions} />;
-  </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </div>
   );
 };
--- src/components/friendList/friendList.jsx
+++ src/components/friendList/friendList.jsx
@@ -1,22 +1,31 @@
 import PropTypes from 'prop-types';
-import css from './friendList.module.css'
+import css from './friendList.module.css';
 import { FriendListItem } from 'components/friendListItem/friendListItem';
 
-export const FriendList = ({friends}) => {
-    return (
+export const FriendList = ({ friends }) => {
+  return (
     <ul className={css.friendList}>
-    {friends.map(({ id, avatar, name, isOnline }) => {
-        return(
-        <FriendListItem key = {id} name={name} avatar={avatar} isOnline ={isOnline}/>)
-    })}
-  </ul>)
-}
+      {friends.map(({ id, avatar, name, isOnline }) => {
+        return (
+          <FriendListItem
+            key={id}
+            name={name}
+            avatar={avatar}
+            isOnline={isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(PropTypes.exact({
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired,
-        id: PropTypes.number.isRequired
-      }),)
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
